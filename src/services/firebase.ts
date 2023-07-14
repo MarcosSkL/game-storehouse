9528d5b2b0139dcba1e3742c4c9ab2e5bc331a36
Marcosskl <marcos.marsk@gmail.com>
2023-07-13 23:38:07 -0300

    feat: .env configurado

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -8,12 +8,12 @@ try {
     app = getApp()
 } catch (error) {
     const firebaseConfig = {
-        apiKey: "AIzaSyAXJk4-yBkpvWNd8ebJrn6q9zqWZKkeUwA",
-        authDomain: "gamestore-bccb3.firebaseapp.com",
-        projectId: "gamestore-bccb3",
-        storageBucket: "gamestore-bccb3.appspot.com",
-        messagingSenderId: "225935679663",
-        appId: "1:225935679663:web:b71040e0d006b99fa1345a"
+        apiKey: process.env.APIKEY,
+        authDomain: process.env.AUTHDOMAIN,
+        projectId: process.env.PROJECTID,
+        storageBucket: process.env.STORAGEBUCKET,
+        messagingSenderId: process.env.MESSAGINGSENDERID,
+        appId: process.env.APPID
     };
 
     app = initializeApp(firebaseConfig);
