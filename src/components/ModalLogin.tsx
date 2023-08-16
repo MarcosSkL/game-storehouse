33013002af05f9055a1f3d7afc1c0e953749a1a4
Marcosskl <marcos.marsk@gmail.com>
2023-08-15 23:28:28 -0300

    Pequenas alterações nas informações de Auth do Google

--- src/components/ModalLogin.tsx
+++ src/components/ModalLogin.tsx
@@ -23,9 +23,18 @@ const ModalLogin = () => {
 
         signInWithPopup(auth, provider)
             .then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential?.accessToken;
+                const user = result.user;
                 setUser(result.user);
+                console.log(user);
             })
             .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+
                 console.log(error)
             })
     }
